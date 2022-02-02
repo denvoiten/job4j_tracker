170723aa4484b37a0988bf382c6313e73cb37dc2
denvoiten <den.voiten@gmail.com>
2022-02-02 11:27:51 +0300

    1. Чтение из консоли. Класс Scanner. [#179514]

---
+++ src/main/java/ru/job4j/io/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.io;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Welcome. What's your name? ");
+        String name = input.nextLine();
+        System.out.println(name + ", glad to see you!");
+    }
+}
