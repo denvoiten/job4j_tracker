6133c1ba19d6e5995c0636bac59865be3b368939
denvoiten <den.voiten@gmail.com>
2022-02-12 10:21:52 +0300

    0. Map, HashMap [#212652 #263340]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("name@gmail.com", "Popov Ivan");
+        map.put("name1@gmail.com", "Sergeev Sergey Serg");
         map.put("name@gmail.com", "Popov Ivan Ivanovich");
         map.put("name1@gmail.com", "Sergeev Sergey Sergeevich");
         map.put("name2@gmail.com", "Ivanov Petr Andreevich");
+
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
