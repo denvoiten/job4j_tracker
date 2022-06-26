3bdf40c253cde02c4624902c949b0e263f4115d6
denvoiten <den.voiten@gmail.com>
2022-06-26 15:24:09 +0300

    5. Генерирование колоды карт. FlatMap [#504873]

---
+++ src/main/java/ru/job4j/stream/Suit.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+public enum Suit {
+    DIAMONDS, HEARTS, SPADES, CLUBS
+}
+
+enum Value {
+    V_2, V_3, V_4, V_5, V_6, V_7, V_8, V_9, V_T, V_J, V_Q, V_K, V_A
+}
+
+class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values())
+                        .map(value -> suit + " " + value))
+                .forEach(System.out::println);
+    }
+}
